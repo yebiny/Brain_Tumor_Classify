f1fbc96d96c3a3956f8f027c9446e86cf39b7500
yebiny <yebinyoun@gmail.com>
2021-08-11 11:21:33 +0900

    split train-test model

--- pre_process.py
+++ pre_process.py
@@ -3,32 +3,53 @@ import pandas as pd
 
 DATA_PATH='/Volumes/BPlus/dataset/miccai2021/rsna-miccai-brain-tumor-radiogenomic-classification'
 TRAIN_DIR='%s/train'%DATA_PATH
+TEST_DIR='%s/test'%DATA_PATH
 TRAIN_LABELS='%s/train_labels.csv'%DATA_PATH
 
+mode='test'
 
-patientIDs = [file for file in os.listdir(TRAIN_DIR) if not file.startswith("._")]
+if mode=='train':
+    DIR=TRAIN_DIR
+    print('Start train mode processing')
+elif mode=='test':
+    DIR=TEST_DIR
+    print('Start test mode processing')
+else: 
+    print("mode error")
+
+
+patientIDs = [file for file in os.listdir(DIR) if not file.startswith("._")]
+print(len(patientIDs))
 img_types=['FLAIR', 'T1w', "T1wCE", "T2w"]
 img_type = img_types[0]
 
-df = pd.read_csv(TRAIN_LABELS)
-p_label, v_label = df['BraTS21ID'], df['MGMT_value']
-p_label, v_label = np.array(p_label), np.array(v_label)
-print("* load label info " , p_label.shape, v_label.shape)
+if mode=='train':
+    df = pd.read_csv(TRAIN_LABELS)
+    p_label, v_label = df['BraTS21ID'], df['MGMT_value']
+    p_label, v_label = np.array(p_label), np.array(v_label)
+    print("* load label info " , p_label.shape, v_label.shape)
 
 x_data, y_data = [], []
-for patientID in patientIDs[400:]:
+for patientID in patientIDs:
     for r in [None, 0]:
         for f in [None, 0, 1]:
-            dcm_list = get_dcm_files(TRAIN_DIR, patientID, img_type)
+            dcm_list = get_dcm_files(DIR, patientID, img_type)
             dcm_arr = load_dcm_imgs_3d(dcm_list, num_imgs=128, img_size=256, rotate=r, flip=f)
             patches = img_2_patches(dcm_arr, crop_size=32)
-            y = [v_label[p_label==int(patientID)][0], p_label[p_label==int(patientID)][0]]
             x_data.append(patches)
-            y_data.append(y)
-    print(y)
+            
+            if mode=='train':
+                y = [v_label[p_label==int(patientID)][0], p_label[p_label==int(patientID)][0]]
+                y_data.append(y)
+    print(patientID)
+
 x_data = np.array(x_data)
 y_data = np.array(y_data)
 print(x_data.shape, y_data.shape)
 
-np.save("x_data.npy", x_data)
-np.save("y_data.npy", y_data)
+if mode=='train':
+    np.save("x_data.npy", x_data)
+    np.save("y_data.npy", y_data)
+else: 
+    np.save("x_test.npy", x_data)
+
